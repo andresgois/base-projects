419d080d908a8941c50a4f3d4ac02ed6c2bf04b3
Andre gois <andre.s.gois3@gmail.com>
2022-03-19 19:59:11 -0300

    :sparkles: rendering template

--- src/modules/accounts/useCase/ServiceController.ts
+++ src/modules/accounts/useCase/ServiceController.ts
@@ -2,7 +2,8 @@ import { Request, Response } from "express";
 
 class ServiceController {
   async handle(request: Request, response: Response): Promise<Response> {
-    return response.status(200).json({ message: "OK4" });
+    response.render("index");
+    return response.send();
   }
 }
 
